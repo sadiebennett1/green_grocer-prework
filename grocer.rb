da1b7cf0b34bdf59d2c3ed6dae9621076749b5cf
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-19 12:46:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,15 @@
 def consolidate_cart(cart)
-  # code here
+  hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |name, price_hash|
+      if hash.include?(name)
+        hash[name][:count] += 1
+      else
+        hash[name] = {:price => price_hash[:price], :clearance => price_hash[:clearance], :count => 1}
+      end
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
@@ -8,8 +18,26 @@ end
 
 def apply_clearance(cart)
   # code here
+  cart.each do |item, price_hash|
+    if price_hash[:clearance] == true
+      price_hash[:price] = (price_hash[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
-def checkout(cart, coupons)
+def checkout(items, coupons)
   # code here
+  cart = consolidate_cart(items)
+  cart1 = apply_coupons(cart, coupons)
+  cart2 = apply_clearance(cart1)
+
+  total = 0
+
+  cart2.each do |name, price_hash|
+    total += price_hash[:price] * price_hash[:count]
+  end
+
+  total > 100 ? total * 0.9 : total
+
 end
